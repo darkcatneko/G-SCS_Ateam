1620134bf37cbd40933caa7446da24a25f7a2b7f
zoeLee <zoe.lee2k@gmail.com>
2022-07-11 20:07:12 +0800

    test0711

---
+++ Assets/Scipts/GridTestScript/GridManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GridClass
+{
+    public Vector2[] pos;
+}
+
+public class GridManager : MonoBehaviour
+{
+    public GridClass[] gridClass = new GridClass[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
