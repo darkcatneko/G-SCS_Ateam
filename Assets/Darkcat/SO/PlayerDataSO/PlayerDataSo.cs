d2f8540fed2dba030435b5914f52219ddd4f96cf
darkcatneko <darkcat7126@gmail.com>
2022-07-11 17:42:18 +0800

    editor

---
+++ Assets/Darkcat/SO/PlayerDataSO/PlayerDataSo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerNum
+{
+    Player1,
+    Player2,
+    Player3,
+    Player4,
+}
+public enum PlayerInput
+{
+    Up,
+    Right,
+    Left,
+    Down,
+    none,
+}
+[CreateAssetMenu(fileName = "NewPlayer", menuName = "Player")]
+public class PlayerDataSo : ScriptableObject
+{
+    public PlayerNum ThisPlayer;
+    public int PlayerMovePoint = 150;
+    public int PlayerPoint=0;
+    public bool Sure = false;
+    public PlayerInput My_Input = PlayerInput.none;
+
+    public void GetFavorite()
+    {
+        PlayerPoint += 2;
+    }
+    public void GetDislike()
+    {
+        PlayerPoint -= 2;
+    }
+}
