c570e6ae6ebbac368533e1a0cc5fe67d5fb05592
SonaruIsuge <bread511206@gmail.com>
2022-07-12 20:02:53 +0800

    Fix game state bugs

--- Assets/Sonaru Developer/Scripts/GameController/StartGameState.cs
+++ Assets/Sonaru Developer/Scripts/GameController/StartGameState.cs
@@ -1,12 +1,15 @@
 ﻿
 
+using UnityEngine;
+
 public class StartGameState : IState
 {
     public GameController Controller { get; set; }
     public void OnStateEnter(GameController controller)
     {
         Controller = controller;
-        Controller.Character.SetOriginPos(2,3);
+        Controller.Character.SetOriginPos(5,1);
+        Debug.Log(Controller.Character.worldMap.GetWorldPosition(5, 1));
     }
 
     public void OnStateStay()
