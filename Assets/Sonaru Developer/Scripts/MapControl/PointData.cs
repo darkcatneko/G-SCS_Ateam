4c7a7fbee307454d143c15783e2ff822715c887e
SonaruIsuge <bread511206@gmail.com>
2022-07-13 03:10:18 +0800

    Add map point items controller

--- Assets/Sonaru Developer/Scripts/MapControl/PointData.cs
+++ Assets/Sonaru Developer/Scripts/MapControl/PointData.cs
@@ -1,20 +1,58 @@
 ﻿
 
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PointData
+[System.Serializable]
+public class PointData : MonoBehaviour
 {
+    public ElementType ElementType;
     public int RowIndex;
     public int ColIndex;
-    public GameObject ItemObject;
-    public bool IsActive;
+    public Action<PointData> OnInactive;
     
-    public PointData(int col, int row, GameObject obj)
+    private bool isActive;
+
+    public bool IsActive
     {
+        get => isActive;
+        set
+        {
+            if(value == false) OnInactive?.Invoke(this);
+            isActive = value;
+        }
+    }
+
+    public void Init(ElementType eType, int col, int row, Action<PointData> onInactive = null)
+    {
+        ElementType = eType;
         RowIndex = row;
         ColIndex = col;
-        ItemObject = obj;
+        isActive = false;
+        OnInactive += onInactive;
+        gameObject.SetActive(isActive);
+    }
+
+    public void Spawn(Vector3 pos)
+    {
+        transform.position = pos;
+        IsActive = true;
+        gameObject.SetActive(IsActive);
+    }
+
+    public void Remove()
+    {
+        IsActive = false;
+        gameObject.SetActive(IsActive);
     }
     
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCharacter"))
+        {
+            Remove();
+        }
+    }
 }
