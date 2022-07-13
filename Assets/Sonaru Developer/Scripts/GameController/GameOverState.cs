8975ad4eb007176ae04e21cceebbe432b77920e6
SonaruIsuge <bread511206@gmail.com>
2022-07-13 11:55:15 +0800

    GameOver clear all player data

--- Assets/Sonaru Developer/Scripts/GameController/GameOverState.cs
+++ Assets/Sonaru Developer/Scripts/GameController/GameOverState.cs
@@ -3,18 +3,30 @@
 
 public class GameOverState : IState
 {
+    private bool enableGameRestartInput = false;
     public GameController Controller { get; set; }
     public void OnStateEnter(GameController controller)
     {
         Controller = controller;
-
+        enableGameRestartInput = false;
+        
         MainGameUIController.instance.ChosenOneIsYou(GetHighestPlayer());
         MainGameUIController.instance.CallEnding();
+        
+        //Delay: 1.5f -> Enable player input to restart game 
+        Controller.StartCoroutine(Controller.DelayDo(1.5f, () => enableGameRestartInput = true ));
     }
 
     public void OnStateStay()
     {
-        
+        if (enableGameRestartInput)
+        {
+            // detect input to restart game.
+            foreach (var player in Controller.players)
+            {
+                player.playerData.clear();
+            }
+        }
     }
 
     public void OnStateExit()
